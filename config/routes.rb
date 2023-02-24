0d7e72f0b44360ec45ef8c48060afee51dcdee10
Linda Cesana <lincesana@gmail.com>
2023-02-24 14:42:40 +0100

    back-end done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookmarks/comment'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 
   resources :lists do
-    resources :bookmarks, only: [:new]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
